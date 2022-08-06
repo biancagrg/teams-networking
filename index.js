a7ef1ff1c74b29d2eca0e1ee95fecad718056580
Bianca Giurgiu <giurgiu.bianca@yahoo.com>
2022-08-06 16:45:26 +0300

    extract getTeamHTML function

--- index.js
+++ index.js
@@ -1,26 +1,21 @@
-function displayTeams(teams) {
-  // var member = document.querySelector("tbody");
-  // for (var i = 0; i < teams.length; i++) {
-  //   member.innerHTML += `<tr><td>${teams[i].promotion}</td><td>${teams[i].members}</td>
-  //   <td>${teams[i].project}</td>
-  //   <td><a href="${teams[i].url}"</a>url</td>
-  //   <td>x e</td>
-  // </tr>`;
-  // }
+function getTeamHTML(team) {
+  return `
+<tr>
+  <td>${team.promotion}</td>
+  <td>${team.members}</td>
+  <td>${team.name}</td>
+  <td>
+    <a href="${team.url}">open</a>
+  </td>
+  <td>x e</td>
+</tr>`;
+}
 
+function displayTeams(teams) {
   // transforma in html
   var teamsHTML = "";
   teams.forEach(function (team) {
-    teamsHTML += `
-      <tr>
-        <td>${team.promotion}</td>
-        <td>${team.members}</td>
-        <td>${team.name}</td>
-        <td>
-          <a href="${team.url}">open</a>
-        </td>
-        <td>x e</td>
-      </tr>`;
+    teamsHTML += getTeamHTML(team);
   });
 
   // afisare
